10920ea01545059347e3e71bb4f18a224d2194a4
MatheusFerrazDev <contatomatheus.oferraz@gmail.com>
2025-04-15 18:41:13 -0300

    att

--- macro-meals/app/page.tsx
+++ macro-meals/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { Header } from "@/components/header/header";
@@ -6,8 +8,10 @@ import { Meals } from "@/components/dashboard/meals";
 export default function Home() {
   const { data: session, status } = useSession();
 
+  // Mostra um carregando enquanto a sessão não está totalmente verificada
   if (status === "loading") return <p>Carregando...</p>;
 
+  // Se o usuário não estiver logado, redireciona para o login
   if (!session) {
     redirect("/sign-in");
   }
