fa062dceb49915d77aa738a22e4b5c1937ffbba1
MatheusFerrazDev <contatomatheus.oferraz@gmail.com>
2025-04-15 21:12:09 -0300

    remove auth

--- macro-meals/app/page.tsx
+++ macro-meals/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 import { Header } from "@/components/header/header";
 import { Meals } from "@/components/dashboard/meals";
 
@@ -12,11 +11,6 @@ export default function Home() {
   console.log("Session Data:", session);
 
   if (status === "loading") return <p>Carregando...</p>;
-
-  if (!session) {
-    console.log("User not logged in, redirecting to /sign-in");
-    redirect("/sign-in");
-  }
 
   return (
     <>
