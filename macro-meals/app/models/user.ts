ff77de8a6cff6599201c03a55485515d9d27d6d5
MatheusFerrazDev <contatomatheus.oferraz@gmail.com>
2025-04-15 22:07:25 -0300

    att

--- macro-meals/app/models/user.ts
+++ macro-meals/app/models/user.ts
@@ -3,14 +3,14 @@ import { Document, model, models, Schema } from "mongoose";
 interface IUser extends Document {
   email: string;
   name: string;
-  password: string;
+  password?: string;
   createdAt: Date;
 }
 
 const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
 });
 
